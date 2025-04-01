cf624d13df9516621e770a37bf924588411b3b73
d2du <me@siddharthjain.in>
2025-04-01 08:27:02 +0530

    updaed to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.utilitygods.com",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [
+    tailwind(), 
+    mdx(), 
+    sitemap(),
+    icon({
+      include: {
+        mdi: ["*"],
+        gridicons: ["*"],
+        ph: ["*"]
+      }
+    })
+  ],
 });
---
+++ src/content/team/sid.md
@@ -0,0 +1,10 @@
+---
+draft: false
+name: "Siddharth"
+title: "Owner/Developer"
+avatar: {
+    src: "https://github.com/Utility-Gods.png",
+    alt: "Siddharth"
+}
+publishDate: "2024-04-01 00:00"
+---
